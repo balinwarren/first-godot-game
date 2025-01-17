6f1d75c4538d4c4095ad16c14ac1e7c368440376
Balin Warren <balin.warren@gmail.com>
2025-01-17 14:13:48 -0700

    Added music and SFX

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 
 @onready var timer: Timer = $Timer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died!")
+	animation_player.play("HurtSound")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animation_player.play("JumpSound")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
